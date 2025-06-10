f5efbdb047859ebc14d931f895de60f1257e5035
laryTgs <laryssatortato99@gmail.com>
2025-06-10 14:33:13 -0300

    up

--- SegundaTela/src/js/script.js
+++ SegundaTela/src/js/script.js
@@ -1,60 +1,60 @@
 // Lista de músicas disponíveis
 const musicas = [
-    {
-        elemento: document.getElementById('musica'),
-        nome: "Hungria - Atmosfera"
-    },
-    {
-        elemento: document.getElementById('musica2'),
-        nome: "Caio luccas - Close friends"
-    },
-    {
-        elemento: document.getElementById('musica3'),
-        nome: "Grupo Revelação - Essência Da Paixão"
-    }
+  {
+    elemento: document.getElementById("musica1"),
+    nome: "Bruno Mars - It Will Rain",
+  },
+  {
+    elemento: document.getElementById("musica2"),
+    nome: "Pedro Capó, Farruko - Calma",
+  },
+  {
+    elemento: document.getElementById("musica3"),
+    nome: "Bruno Mars - Just the Way You Are",
+  },
 ];
 
 let indiceAtual = 0;
 let isPlaying = false;
 
-const playButton = document.getElementById('playButton');
+const playButton = document.getElementById("playButton");
 
 function pararMusicas() {
-    musicas.forEach(musica => {
-        if (musica.elemento) {
-            musica.elemento.pause();
-            musica.elemento.currentTime = 0;
-        }
-    });
+  musicas.forEach((musica) => {
+    if (musica.elemento) {
+      musica.elemento.pause();
+      musica.elemento.currentTime = 0;
+    }
+  });
 }
 
 function tocarProximaMusica() {
-    pararMusicas();
-    const musicaAtual = musicas[indiceAtual];
-    if (musicaAtual && musicaAtual.elemento) {
-        musicaAtual.elemento.play();
-        // Atualiza o índice para a próxima música
-        indiceAtual = (indiceAtual + 1) % musicas.length;
-    }
+  pararMusicas();
+  const musicaAtual = musicas[indiceAtual];
+  if (musicaAtual && musicaAtual.elemento) {
+    musicaAtual.elemento.play();
+    // Atualiza o índice para a próxima música
+    indiceAtual = (indiceAtual + 1) % musicas.length;
+  }
 }
 
-playButton.addEventListener('click', () => {
-    if (isPlaying) {
-        pararMusicas();
-        playButton.textContent = '🎶 Reproduzir Música 🎶';
-        isPlaying = false;
-    } else {
-        tocarProximaMusica();
-        playButton.textContent = '🎶 Pausar Música 🎶';
-        isPlaying = true;
-    }
+playButton.addEventListener("click", () => {
+  if (isPlaying) {
+    pararMusicas();
+    playButton.textContent = "🎶 Reproduzir Música 🎶";
+    isPlaying = false;
+  } else {
+    tocarProximaMusica();
+    playButton.textContent = "🎶 Pausar Música 🎶";
+    isPlaying = true;
+  }
 });
 
 // Quando uma música terminar, tocar a próxima
-musicas.forEach(musica => {
-    if (musica.elemento) {
-        musica.elemento.addEventListener('ended', () => {
-            tocarProximaMusica();
-        });
-    }
-}); 
+musicas.forEach((musica) => {
+  if (musica.elemento) {
+    musica.elemento.addEventListener("ended", () => {
+      tocarProximaMusica();
+    });
+  }
+});
